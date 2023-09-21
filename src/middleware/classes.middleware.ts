da60a3f8f3673a593c61ae4f6a47c413ec3c62f4
ahmedmab <ahmedmab87@gmail.com>
2023-09-21 01:24:52 +0100

    cycles midlware dates of weeks DONE

--- src/middleware/classes.middleware.ts
+++ src/middleware/classes.middleware.ts
@@ -1,16 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function classeHandler(req: Request, res: Response, next: NextFunction) {
-    let classes: any[] = []
-    for (const nvAr of req.body.classes) {
-        for (const classe of nvAr) {
-            classes.push(classe)
-        }
+    if (Array.isArray(req.body.classes[0])) {
+        let classes: any[] = []
+        for (const nvAr of req.body.classes) {
+            for (const classe of nvAr) {
+                classes.push(classe)
+            }
 
+        }
+        classes = classes.map(({ name, nvId, nvName }) => ({ name, nvId, nvName }))
+        classes.sort((a, b) => a.nvId - b.nvId);
+        req.body.classes = classes
     }
-    classes = classes.map(({ name, nvId, nvName }) => ({ name, nvId, nvName }))
-    classes.sort((a, b) => a.nvId - b.nvId);
-    req.body.classes = classes
-    console.log(`Request...`, req.body.classes);
+
     next();
 };
--- src/middleware/cycles.middleware.ts
+++ src/middleware/cycles.middleware.ts
@@ -2,30 +2,36 @@ import { Request, Response, NextFunction } from 'express';
 import * as moment from "moment";
 
 export function cycleHandler(req: Request, res: Response, next: NextFunction) {
+
     for (const cycle of req.body.cycles) {
         let weeks: any[] = []
-        // let weekNumber = Math.round(((new Date(cycle.endDate).getTime())- new Date(cycle.startDate).getTime()) / (7 * 24 * 60 * 60 * 1000))
-        var startDate = moment(new Date(cycle.startDate))
-        // .isoWeekday(8);
-        // if (startDate.date() == 8) {
-        //     startDate = startDate.isoWeekday(-6)
-        // }
+        var startDate = moment(cycle.startDate).weekday(8)
 
-        // var today = moment().isoWeekday('Sunday');
-        while (startDate.isBefore(new Date(cycle.endDate))) {            
-            let startDateWeek = startDate.isoWeekday('Monday').format('DD-MM-YYYY');
-            let endDateWeek = startDate.isoWeekday('Sunday').format('DD-MM-YYYY');
-            var week = [startDateWeek, endDateWeek]
+        while (startDate.isBefore(new Date(cycle.endDate))) {
+            let startDateWeek = startDate.format('DD-MM-YYYY');
+            let endDateWeek = startDate.clone().add(6, 'days');
+            var week = [startDateWeek, endDateWeek.format('DD-MM-YYYY')]
+            if (endDateWeek.isSameOrBefore(new Date(cycle.endDate))) {
+                weeks.push(week);
+            }
             startDate.add(7, 'days');
+
+        }
+        if (true) {
+            let firstMonday = moment(cycle.startDate).weekday(7).format('DD-MM-YYYY')
+            week = [moment(cycle.startDate).format('DD-MM-YYYY'), firstMonday]
+            weeks.unshift(week);
+        }
+        if (true) {
+            let lastMonday = moment(cycle.endDate).weekday(1).format('DD-MM-YYYY')
+            week = [lastMonday, moment(new Date(cycle.endDate)).format('DD-MM-YYYY')]
             weeks.push(week);
         }
-        // cycle.weeks.push(week);
+        // cycle.weeks.push(week); 
         cycle.weeks = weeks
-        console.log(cycle);
 
     }
     // cycles = cycles.map(({ name, nvId, nvName }) => ({ name, nvId, nvName }))
     // req.body.cycles = cycles
-    // console.log(`Cycle Request...`, req.body.cycles);
     next();
 };
