c0ef1a3941a3649a713d665f3ecb781fa9c6625a
ahmedmab <ahmedmab87@gmail.com>
2023-10-01 18:00:31 +0100

    try to deploy

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     origin: ['http://localhost:4200'],
     credentials: true
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
