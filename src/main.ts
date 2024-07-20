fa1126562a178e3dad327e0c5229989512770a61
ahmed <ahmedmab87@gmail.com>
2024-07-20 17:36:38 +0100

    set firebase functions to eps-io project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import { initializeApp, applicationDefault } from 'firebase-admin/app';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  initializeApp({
+    credential: applicationDefault(),
+    projectId: 'eps-api',
+  });
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.enableCors();
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
